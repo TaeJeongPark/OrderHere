bb217f0ac3b387415d848a45a7fb8d302211bb39
YHSprv <yunasong519.prv@gmail.com>
2022-05-21 19:04:12 +0900

    Test.java 변경

--- src/orderhere/order/Test.java
+++ src/orderhere/order/Test.java
@@ -1,10 +1,15 @@
 package orderhere.order;
 
+import orderhere.order.cart.Cart;
+
 public class Test {
-
 	public static void main(String[] args) {
-		new OrderList();
-		System.out.println("  ");
+//		new Cart();
+//		new OrderList();
+		new Cart();
+//		new Payment();
+//		new OrderDetails();
+		System.out.println("   ");
+		
 	}
-
 }
